ae2795714b696040bbe5b62c9f787d6d891b1811
Appinventiv <jay.pandey45@gmail.com>
2019-07-24 18:48:34 +0530

    update

--- home/views.py
+++ home/views.py
@@ -5,3 +5,12 @@ def index(request):
 
 def about(request):
     return render(request,'login/about.html')
+
+def services(request):
+    return render(request, 'login/services.html')
+
+def contact(request):
+    return render(request, 'login/contact.html')
+
+def gallery(request):
+    return render(request, 'login/gallery.html')
