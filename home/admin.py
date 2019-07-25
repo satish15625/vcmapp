3b211bc70812690c49a9463af91959062b92aecf
Appinventiv <jay.pandey45@gmail.com>
2019-07-26 00:23:22 +0530

    update

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Banner,ContactHeader,ProfessionTeam
+from .models import Banner,ContactHeader,ProfessionTeam,HappyClients,ServicesOffered
 # Register your models here.
 admin.site.site_header = 'Villagers Group Admin Panel'
 
-admin.register(Banner,ContactHeader,ProfessionTeam)(admin.ModelAdmin)
+admin.register(Banner,ContactHeader,ProfessionTeam,HappyClients,ServicesOffered)(admin.ModelAdmin)
--- home/models.py
+++ home/models.py
@@ -1,32 +1,38 @@
 from django.db import models
 
 # Create your banner modee
+
+
 class Banner(models.Model):
+    """
+    model class for storing banner image 
+
+    """
     banner_img = models.ImageField(upload_to='media')
     banner_title = models.CharField(max_length=100)
-    banner_desc = models.TextField() 
+    banner_desc = models.TextField()
     banner_right_desc = models.TextField()
-    read_more_desc =  models.TextField()
+    read_more_desc = models.TextField()
 
     class Meta:
         db_table = 'vg_banner'
 
-    
+
 class ProfessionTeam(models.Model):
     """
     model class for handling contact header informations
 
     """
-    avatar       = models.ImageField(upload_to = "media")
-    name         = models.CharField(max_length = 50)
-    designation  = models.CharField(max_length = 25)
-    description  = models.TextField(null=True)
+    avatar = models.ImageField(upload_to="media")
+    name = models.CharField(max_length=50)
+    designation = models.CharField(max_length=25)
+    description = models.TextField(null=True)
     phone_number = models.BigIntegerField()
     country_code = models.CharField(max_length=6)
     contact_email = models.EmailField()
-    facebook_url  = models.URLField()
-    twitter_url  = models.URLField()
-    insta_url    = models.URLField()
+    facebook_url = models.URLField()
+    twitter_url = models.URLField()
+    insta_url = models.URLField()
     linkedin_url = models.URLField()
 
     class Meta:
@@ -41,11 +47,35 @@ class ContactHeader(models.Model):
     phone_number = models.BigIntegerField()
     country_code = models.CharField(max_length=6)
     contact_email = models.EmailField()
-    facebook_url  = models.URLField()
-    twitter_url  = models.URLField()
-    insta_url    = models.URLField()
+    facebook_url = models.URLField()
+    twitter_url = models.URLField()
+    insta_url = models.URLField()
     linkedin_url = models.URLField()
 
     class Meta:
         db_table = 'vg_contact_header'
 
+
+class ServicesOffered(models.Model):
+    """
+    service model to store the type of service offered by the company
+
+    """
+    service_name = models.CharField(max_length=30)
+    service_desc = models.TextField()
+    service_image = models.ImageField(upload_to='media')
+    service_icon = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = 'vg_service_offered'
+
+
+class HappyClients(models.Model):
+    client_name = models.CharField(max_length=50)
+    designation = models.CharField(max_length=25)
+    avatar = models.ImageField(upload_to='media')
+    rating = models.IntegerField()
+    description = models.TextField()
+
+    class Meta:
+        db_table  = 'vg_happy_clients'
