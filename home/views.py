84de9f7266083b66e1485df9d2a90a27e42f8932
Appinventiv <jay.pandey45@gmail.com>
2019-07-26 19:27:30 +0530

    update

--- home/views.py
+++ home/views.py
@@ -5,23 +5,34 @@ view file for handling rendering action
 
 
 from django.shortcuts import render
-from .models import Banner,ProfessionTeam
+from .models import Banner, ProfessionTeam, ServicesOffered, HappyClients
+
 
 def index(request):
 
-    banner = Banner.objects.all() 
+    # home page for showing banner list and image
+    banner = Banner.objects.all()
+
     professional = ProfessionTeam.objects.all()
 
-    return render(request,'login/index.html',{'banner':banner,'professional': professional})
+    services = ServicesOffered.objects.all()
+
+    happyclients = HappyClients.objects.all()
+
+    return render(request, 'login/index.html', {'banner': banner, 'professional': professional, 'services' : services, 'happyclient' : happyclients})
+
 
 def about(request):
-    return render(request,'login/about.html')
+    return render(request, 'login/about.html')
+
 
 def services(request):
     return render(request, 'login/services.html')
 
+
 def contact(request):
     return render(request, 'login/contact.html')
+
 
 def gallery(request):
     return render(request, 'login/gallery.html')
