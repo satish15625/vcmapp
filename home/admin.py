86acee412ffe670ad63c4e16f4048f217a3bcc83
Appinventiv <satish15625@gmail>
2019-08-06 17:57:21 +0530

    update

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Banner,ContactHeader,ProfessionTeam,HappyClients,VendorAds,ServicesOffered,SubscriptionPlans,AboutUs,GalleryContent,OurStatistics,Logo,About_Details
 # Register your models here.
-
 admin.site.site_header = 'Villagers Group Admin Panel'
 admin.site.site_title="Villagers Group Admin Login"
 admin.site.index_title ="Welcome to Villagersgroup Admin"
